315d19dbfa0332fd7e575c8454d2ba7ce01b17d9
Spencer Lyon <spencerlyon2@gmail.com>
2012-11-15 16:58:55 -0700

    Did problem 26

---
+++ SL_Problem26.py
@@ -0,0 +1,23 @@
+"""
+Created Nov 15, 2012
+
+Author: Spencer Lyon
+
+Project Euler Problem 26
+
+Note that I got the idea from the following website:
+
+    http://primes.utm.edu/glossary/xpage/PeriodOfADecimal.html
+"""
+import sympy as sym
+import numpy as np
+
+# The number must be prime and I am guessing it is big so start with highest.
+primes = np.asarray(list(sym.primerange(1, 1000)))[::-1][:-3]
+
+periods = np.asarray([sym.ntheory.n_order(10, i)for i in primes])
+the_max = np.argmax(periods)
+
+ans = primes[the_max]
+
+print "The answer is: ", ans
